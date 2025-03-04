31ae39952e49f1c80a01732423a272d8426f4b6e
GRIN-96 <bnmnil96@gmail.com>
2025-03-05 01:12:47 +0900

    Docs: 백준 문제풀이 - 1546 - 11720

---
+++ src/main/java/backjoon/backjoon_11720/Main.java
@@ -0,0 +1,22 @@
+package backjoon.backjoon_11720;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String number = br.readLine();
+        String[] split = number.split("");
+        int result = 0;
+
+        for(int i=0; i<N; i++){
+            result += Integer.parseInt(split[i]);
+        }
+
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/backjoon/backjoon_1546/Main.java
@@ -0,0 +1,25 @@
+package backjoon.backjoon_1546;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String number = br.readLine();
+        String[] split = number.split(" ");
+        int maxScore = 0;
+        int sumScore = 0;
+
+        for(int i=0; i<N; i++){
+            int score = Integer.parseInt(split[i]);
+            maxScore = Math.max(maxScore, score);
+            sumScore += score;
+        }
+
+        System.out.println(sumScore * 100 / maxScore / N);
+    }
+}
